51f96bab547f3962d3281e1311d1b61499f7c693
kamem <develo0@gmail.com>
2020-02-06 03:55:05 +0900

    no message

--- example/example1/js/index.js
+++ example/example1/js/index.js
@@ -4,7 +4,7 @@ import '../../../jquery'
 const $nav = $('.gnav > ul > *')
 
 $nav.each(function () {
-  $(this).parallaxTiming({ target: $(this).find('a').attr('href'), class: 'aaaa' })
+  $(this).parallaxTiming({ target: $(this).find('a').attr('href') })
 })
 
 $('#timing').parallaxTiming()
