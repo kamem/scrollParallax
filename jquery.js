30880c9966012c7d4b0d979d059f0f3c24291833
kamem <develo0@gmail.com>
2020-09-30 00:00:59 +0900

    0.1.1

--- jquery.js
+++ jquery.js
@@ -2,7 +2,7 @@
  * scroll-parallax-effect
  * Implementing parallax effect by utilizing various events of scroll.
  * https://github.com/kamem/scrollParallax.git
- * @version 0.1.0
+ * @version 0.1.1
  * @license Released under MIT license
  * @author kamem
  */
